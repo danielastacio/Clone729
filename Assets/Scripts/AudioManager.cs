1f4d4fb78139248782a60b3f29204e189f80c529
Daniel Astacio <danielastacio91@gmail.com>
2022-05-04 16:14:14 -0400

    Put it in a namespace

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,52 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using AK;
-public class AudioManager : MonoBehaviour
+
+namespace Metroidvaniajam.Audio
 {
-    private static AudioManager instance;
-
-    public static AudioManager Instance { get { return instance; } }
-
-    private void Awake()
+    public class AudioManager : MonoBehaviour
     {
-        if (instance != null && instance != this)
+        private static AudioManager instance;
+
+        public static AudioManager Instance { get { return instance; } }
+
+        private void Awake()
         {
-            Destroy(this.gameObject);
+            if (instance != null && instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                instance = this;
+            }
         }
-        else
+        public virtual void WwiseAudio(AK.Wwise.Event wwiseEvent)
         {
-            instance = this;
+            wwiseEvent.Post(this.gameObject);
         }
-    }
-    public virtual void WwiseAudio(AK.Wwise.Event wwiseEvent)
-    {
-        wwiseEvent.Post(this.gameObject);
-    }
 
-    public virtual void WwiseAudio(AK.Wwise.State wwiseState)
-    {
-        wwiseState.SetValue();
-    }
+        public virtual void WwiseAudio(AK.Wwise.State wwiseState)
+        {
+            wwiseState.SetValue();
+        }
 
-    public virtual void WwiseAudio(AK.Wwise.Switch wwiseSwitch, GameObject gameObject = null)
-    {
-        wwiseSwitch.SetValue(gameObject);
-    }
+        public virtual void WwiseAudio(AK.Wwise.Switch wwiseSwitch, GameObject gameObject = null)
+        {
+            wwiseSwitch.SetValue(gameObject);
+        }
 
-    public virtual void WwiseAudio(AK.Wwise.RTPC wwiseRTPC, float value, GameObject gameObject = null)
-    {
-        wwiseRTPC.SetValue(gameObject, value);
-    }
+        public virtual void WwiseAudio(AK.Wwise.RTPC wwiseRTPC, float value, GameObject gameObject = null)
+        {
+            wwiseRTPC.SetValue(gameObject, value);
+        }
 
-    // Start is called before the first frame update
-    void Start()
-    {
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
