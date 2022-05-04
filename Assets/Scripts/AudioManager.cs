b2ce216e63c68a5d289c9f6dbef308f8f29c79da
Daniel Astacio <danielastacio91@gmail.com>
2022-05-04 16:12:44 -0400

    Overload method for wwise audio actions

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using AK;
 public class AudioManager : MonoBehaviour
 {
     private static AudioManager instance;
@@ -19,11 +19,30 @@ public class AudioManager : MonoBehaviour
             instance = this;
         }
     }
+    public virtual void WwiseAudio(AK.Wwise.Event wwiseEvent)
+    {
+        wwiseEvent.Post(this.gameObject);
+    }
+
+    public virtual void WwiseAudio(AK.Wwise.State wwiseState)
+    {
+        wwiseState.SetValue();
+    }
+
+    public virtual void WwiseAudio(AK.Wwise.Switch wwiseSwitch, GameObject gameObject = null)
+    {
+        wwiseSwitch.SetValue(gameObject);
+    }
+
+    public virtual void WwiseAudio(AK.Wwise.RTPC wwiseRTPC, float value, GameObject gameObject = null)
+    {
+        wwiseRTPC.SetValue(gameObject, value);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
