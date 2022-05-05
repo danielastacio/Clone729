5c74a14a932f8e9bd18a330d02caf618015090db
Tyler <tylerditlow@gmail.com>
2022-05-04 23:55:19 -0400

    Added Coroutines to control door animation.

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -27,11 +27,7 @@ public class DoorController : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // TODO: Set up door to transition from closed to open instead of jumping
-            transform.position = new Vector3(
-                transform.position.x,
-                -2f,
-                transform.position.z);
+            StartCoroutine(OpenDoor())
         }
     }
 
@@ -40,10 +36,7 @@ public class DoorController : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             // TODO: Set up door to transition from open to closed instead of jumping
-            transform.position = new Vector3(
-                transform.position.x,
-                2f,
-                transform.position.z);
+            StartCoroutine(CloseDoor())
             
             // TODO: If player enters from a 2D space, and goes back, camera still transitions to 3D
             // TODO: Need to check where the player is coming from and if that change needs to happen.
@@ -60,5 +53,23 @@ public class DoorController : MonoBehaviour
 
             _playerController.isIn3DSpace = !_playerController.isIn3DSpace;
         }
+    }
+
+    IEnumerator OpenDoor()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            Mathf.Lerp(_closedPos, _openPos, _transitionTime),
+            transform.position.z);
+        yield return null;
+    }
+
+    IEnumerator CloseDoor()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            Mathf.Lerp(_openPos, _closedPos, _transitionTime,
+            transform.position.z);
+        yield return null;
     }
 }
