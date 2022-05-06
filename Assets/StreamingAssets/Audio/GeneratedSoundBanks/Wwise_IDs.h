06187e995b7d287d179d46d56e338260d261ede7
Daniel Astacio <danielastacio91@gmail.com>
2022-05-06 11:02:30 -0400

    Wwise steam events and assets

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_STEAM = 2885688842U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
