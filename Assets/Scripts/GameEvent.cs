5269f79f6a2b6a6209b5bb08bcbc23d013cd159b
Daniel Astacio <danielastacio91@gmail.com>
2022-05-10 16:09:43 -0400

    Custom game event system
    This was created by Jason Weimann to help simplify game events by making them draggable and droppable. It's modular, reusable, and easy to implement. The game event script is a scriptable object, which makes it simple to create new game event assets and provides a visual hierarchy for all your game events via your project folder structure.

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game Event", fileName = "New Game Event")]
+public class GameEvent : ScriptableObject
+{
+    HashSet<GameEventListener> listeners = new();
+
+    public void Invoke()
+    {
+        foreach (var globalEventListener in listeners)
+            globalEventListener.RaiseEvent();
+    }
+
+    public void Register(GameEventListener gameEventListener) => listeners.Add(gameEventListener);
+    public void DeRegister(GameEventListener gameEventListener) => listeners.Remove(gameEventListener);
+}
+
