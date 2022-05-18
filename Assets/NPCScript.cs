ad26affc2bade6b9d4c04adb00663fb552faef7f
Moe <seomwe@gmail.com>
2022-05-18 21:43:40 +0300

    NPC Script
    Work in progress NPC Script

---
+++ Assets/NPCScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCScript : MonoBehaviour
+{
+    //Public Inspector variables here
+
+    [SerializeField]
+    float speed;
+
+    //Private Variables
+    RaycastHit2D hit;
+    //Component variables
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+       
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hit = Physics2D.Raycast(transform.position,new Vector2(1,-1),3);
+       if (hit)
+        {
+            Debug.Log("Object Hit?: " + hit+", What was hit?" + hit.collider.name);
+            Debug.DrawRay(transform.position, new Vector2(1, -1) * 2, Color.green, 60);
+            transform.Translate(Vector2.right * speed/10);
+
+            Debug.Log(h);
+        }
+       
+    }
+}   
