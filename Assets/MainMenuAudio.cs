bcb3d4b9376196781b8d0d2ed01d8f39aec996e1
Daniel Astacio <danielastacio91@gmail.com>
2022-05-18 10:25:16 -0400

    Update MainMenuAudio.cs

--- Assets/MainMenuAudio.cs
+++ Assets/MainMenuAudio.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MainMenuAudio : MonoBehaviour
 {
-    UI_MainMenu mainMenu;
+    public AK.Wwise.Event menuEnter, menuReturn, menuStartGame;
+    public AK.Wwise.Bank mainMenuBank;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        mainMenuBank.Load();
+    }
     void Start()
     {
+        UI_MainMenu.Instance.BTN_StartNewGame.onClick.AddListener(PlayStartGame);
+        UI_MainMenu.Instance.BTN_LoadGame.onClick.AddListener(PlayMenuEnter);
+        UI_MainMenu.Instance.BTN_ShowLoadUI.onClick.AddListener(PlayMenuEnter);
+        UI_MainMenu.Instance.BTN_ShowCreditsUI.onClick.AddListener(PlayMenuEnter);
+        UI_MainMenu.Instance.BTN_ReturnToMainMenu.onClick.AddListener(PlayMenuEnter);
+        UI_MainMenu.Instance.BTN_QuitGame.onClick.AddListener(PlayMenuReturn);
+
+       // PlayStartGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    void PlayMenuEnter()
     {
-        
+        menuEnter.Post(gameObject);
+    }
+
+    void PlayMenuReturn()
+    {
+        menuReturn.Post(gameObject);
+    }
+
+    void PlayStartGame()
+    {
+        menuStartGame.Post(gameObject);
     }
 }
+
