73c0758c81c80f8b9ae465d15bd9cd821a11c481
Tyler Ditlow <59483267+TylerD94@users.noreply.github.com>
2022-05-17 21:05:05 -0400

    Finalized spider-bot ability to walk on walls/ceiling

--- Assets/Scripts/SpiderBot.cs
+++ Assets/Scripts/SpiderBot.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 using Interfaces;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
-// TODO: Set up ability for spider-bot to walk on walls
 // TODO: Set up die state
 
 public class SpiderBot : MonoBehaviour, IDamageable
@@ -16,22 +16,43 @@ public class SpiderBot : MonoBehaviour, IDamageable
         Idle,
         Die
     };
+
+    private enum Positions
+    {
+        Ground,
+        Ceiling,
+        LeftWall,
+        RightWall
+    };
     
+    private readonly Dictionary<Positions, Vector3> _startingRotations = new Dictionary<Positions, Vector3>()
+    {
+        {Positions.Ground, new Vector3(0, 0, 0)},
+        {Positions.Ceiling, new Vector3(0, 0, 180)},
+        {Positions.LeftWall, new Vector3(0, 0, 270)},
+        {Positions.RightWall, new Vector3(0, 0, 90)}
+    };
+
     private EnemyState _currentState = EnemyState.Idle;
 
     [Header("Stats")] 
     public float maxHp;
     private float _currentHp;
     public float attackDamage;
-    
-    [Header("Movement")]
+
+    [Header("Movement")] 
     public float speed;
-    public float gcRaycastDist;
-    public bool facingRight = true;
-    public bool walkOnCeiling;
+    private float _horizSpeed;
+    private float _vertSpeed;
+    private float _gcRaycastDist;
+    [SerializeField] private bool facingRight;
+    [SerializeField] private Positions startingPosition;
+    private Vector3 _startingRotation;
+    private Vector2 _groundCheckDirection;
+    private Vector2 _wallCheckDirection;
     public LayerMask whatIsGround;
     public LayerMask whatIsWall;
-    
+
     // Controls for stopping and restarting movement
     [Header("Stop Movement")]
     public float minTimeBeforeNextStop;
@@ -52,11 +73,8 @@ public class SpiderBot : MonoBehaviour, IDamageable
         _rb = GetComponent<Rigidbody2D>();
         _groundCheck = transform.GetChild(1).transform;
 
-        if (!facingRight)
-        {
-            Flip(-180);
-            speed *= -1;
-        }
+        SetStartingRotation();
+        SetRotationAndSpeed();
     }
 
     private void FixedUpdate()
@@ -73,83 +91,122 @@ public class SpiderBot : MonoBehaviour, IDamageable
         }
     }
 
-    private IEnumerator WalkCycle()
+    private void SetStartingRotation()
     {
-        while (_currentState == EnemyState.Idle)
+        switch (startingPosition)
         {
-            var stopChance = Random.Range(0f, 100f);
-            
-            if (stopChance < 10 && _timeSinceLastStop > _timeBeforeNextStop)
-            {
-                yield return StartCoroutine(Stop());
-            }
-            else
-            {
-                _timeSinceLastStop += Time.deltaTime;
-            }
-            
-            CheckForGround();
-            CheckForWall();
-
-            _rb.velocity = new Vector2(speed, 0);
-
-            yield return null;
+            case Positions.Ground:
+                _startingRotation = _startingRotations[Positions.Ground];
+                _groundCheckDirection = Vector2.down;
+                break;
+            case Positions.Ceiling:
+                _startingRotation = _startingRotations[Positions.Ceiling];
+                _groundCheckDirection = Vector2.up;
+                break;
+            case Positions.LeftWall:
+                _startingRotation = _startingRotations[Positions.LeftWall];
+                _groundCheckDirection = Vector2.left;
+                break;
+            case Positions.RightWall:
+                _startingRotation = _startingRotations[Positions.RightWall];
+                _groundCheckDirection = Vector2.right;
+                break;
         }
+        transform.eulerAngles = _startingRotation;
+    }
+
+    private void SetRotationAndSpeed()
+    {
+        if ((facingRight && startingPosition == Positions.Ground) || 
+            (!facingRight && startingPosition == Positions.Ceiling))
+        {
+            _horizSpeed = speed;
+            SetHorizontalRotation();
+            SetWallCheck(Vector2.right);
+        }
+        else if ((!facingRight && startingPosition == Positions.Ground) ||
+                 facingRight && startingPosition == Positions.Ceiling)
+        {
+            _horizSpeed = -speed;
+            SetHorizontalRotation();
+            SetWallCheck(Vector2.left);
+        }
+        else if ((facingRight && startingPosition == Positions.RightWall) || 
+                 !facingRight && startingPosition == Positions.LeftWall)
+        {
+            _vertSpeed = speed;
+            SetVerticalRotation();
+            SetWallCheck(Vector2.up);
+        }
+        else if ((!facingRight && startingPosition == Positions.RightWall) || 
+                 facingRight && startingPosition == Positions.LeftWall)
+        {
+            _vertSpeed = -speed;
+            SetVerticalRotation();
+            SetWallCheck(Vector2.down);
+        }
+    }
+    
+    private void SetHorizontalRotation()
+    {
+        if (facingRight)
+        {
+            transform.eulerAngles = new Vector3(_startingRotation.x, 0, _startingRotation.z);
+        }
+        else if (!facingRight)
+        {
+            transform.eulerAngles = new Vector3(_startingRotation.x, 180, _startingRotation.z);
+        }
+    }
+
+    private void SetVerticalRotation()
+    {
+        if (facingRight)
+        {
+            transform.eulerAngles = new Vector3(0, _startingRotation.y, _startingRotation.z);
+        }
+        else if (!facingRight)
+        {
+            transform.eulerAngles = new Vector3(180, _startingRotation.y, _startingRotation.z);
+        }
+    }
+    
+    private void SetWallCheck(Vector2 direction)
+    {
+        _wallCheckDirection = direction;
     }
 
     private void CheckForGround()
     {
-        // If spider-bot is on ceiling, this sets raycast to shoot up instead of down.
-        var groundCheckDirection = walkOnCeiling ? Vector2.up : Vector2.down;
         var groundCheck = 
-            Physics2D.Raycast(_groundCheck.position, groundCheckDirection, gcRaycastDist, whatIsGround);
+            Physics2D.Raycast(_groundCheck.position, _groundCheckDirection, _gcRaycastDist, whatIsGround);
 
-        if (!groundCheck && facingRight)
+        if (!groundCheck)
         {
-            Flip(-180);
+            Flip();
         }
-        else if (!groundCheck && !facingRight)
-        {
-            Flip(0);
-        }
+
     }
 
     private void CheckForWall()
     {
-        // Depending on facing direction, raycast will point forward
-        var wallCheckDirection = facingRight ? Vector2.right : Vector2.left;
         var wallCheck =
-            Physics2D.Raycast(_groundCheck.position, wallCheckDirection, gcRaycastDist, whatIsWall);
+            Physics2D.Raycast(_groundCheck.position, _wallCheckDirection, _gcRaycastDist, whatIsWall);
+        
         if (wallCheck && facingRight)
         {
-            Flip(-180);
+            Flip();
         }
         else if (wallCheck && !facingRight)
         {
-            Flip(0);
+            Flip();
         }
     }
     
-    private void Flip(int flipAmount)
+    private void Flip()
     {
-        transform.eulerAngles = new Vector3(0, -flipAmount, 0);
         facingRight = !facingRight;
-        speed *= -1;
-    }
-
-    private IEnumerator Stop()
-    {
-        var stopTime = Random.Range(minStopTime, maxStopTime);
-        var currentSpeed = speed;
-        speed = 0;
-        _rb.Sleep();
-       
-        _timeSinceLastStop = 0f;
-        _timeBeforeNextStop = Random.Range(minTimeBeforeNextStop, maxTimeBeforeNextStop);
-
-        yield return new WaitForSeconds(stopTime);
-        
-        speed = currentSpeed;
+        SetRotationAndSpeed();
     }
 
     public void TakeDamage(float damage)
@@ -168,5 +225,44 @@ public class SpiderBot : MonoBehaviour, IDamageable
             IDamageable damageable = other.gameObject.GetComponent<IDamageable>();
             damageable.TakeDamage(attackDamage);
         }
+    }
+    
+    private IEnumerator WalkCycle()
+    {
+        while (_currentState == EnemyState.Idle)
+        {
+            var stopChance = Random.Range(0f, 100f);
+            
+            if (stopChance < 10 && _timeSinceLastStop > _timeBeforeNextStop)
+            {
+                yield return StartCoroutine(Stop());
+            }
+            else
+            {
+                _timeSinceLastStop += Time.deltaTime;
+            }
+            
+            CheckForGround();
+            CheckForWall();
+
+            _rb.velocity = new Vector2(_horizSpeed, _vertSpeed);
+
+            yield return null;
+        }
+    }
+    
+    private IEnumerator Stop()
+    {
+        var stopTime = Random.Range(minStopTime, maxStopTime);
+        var currentSpeed = _rb.velocity;
+        _rb.velocity = Vector2.zero;
+        _rb.Sleep();
+       
+        _timeSinceLastStop = 0f;
+        _timeBeforeNextStop = Random.Range(minTimeBeforeNextStop, maxTimeBeforeNextStop);
+
+        yield return new WaitForSeconds(stopTime);
+        
+        _rb.velocity = currentSpeed;
     }
 }
