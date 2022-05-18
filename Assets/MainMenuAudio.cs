6e20cb10d0216c25f497e0910f4b65826cd86bc3
Daniel Astacio <danielastacio91@gmail.com>
2022-05-18 14:48:06 -0400

    Update MainMenuAudio.cs

--- Assets/MainMenuAudio.cs
+++ Assets/MainMenuAudio.cs
@@ -2,39 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
-
 public class MainMenuAudio : MonoBehaviour
 {
-    public AK.Wwise.Event menuEnter, menuReturn, menuStartGame;
-    public AK.Wwise.Bank mainMenuBank;
+    [SerializeField]
+    private AK.Wwise.Event menuEnter, menuReturn, menuSelect, menuStartGame;
+
+    [SerializeField]
+    private AK.Wwise.Bank mainMenuBank;
     // Start is called before the first frame update
     private void Awake()
     {
         mainMenuBank.Load();
     }
-    void Start()
+
+    public void PlayMenuSelect()
     {
-        UI_MainMenu.Instance.BTN_StartNewGame.onClick.AddListener(PlayStartGame);
-        UI_MainMenu.Instance.BTN_LoadGame.onClick.AddListener(PlayMenuEnter);
-        UI_MainMenu.Instance.BTN_ShowLoadUI.onClick.AddListener(PlayMenuEnter);
-        UI_MainMenu.Instance.BTN_ShowCreditsUI.onClick.AddListener(PlayMenuEnter);
-        UI_MainMenu.Instance.BTN_ReturnToMainMenu.onClick.AddListener(PlayMenuEnter);
-        UI_MainMenu.Instance.BTN_QuitGame.onClick.AddListener(PlayMenuReturn);
-
-       // PlayStartGame();
+        menuSelect.Post(gameObject);
     }
-
-    void PlayMenuEnter()
+    public void PlayMenuEnter()
     {
         menuEnter.Post(gameObject);
     }
 
-    void PlayMenuReturn()
+    public void PlayMenuReturn()
     {
         menuReturn.Post(gameObject);
     }
 
-    void PlayStartGame()
+    public void PlayStartGame()
     {
         menuStartGame.Post(gameObject);
     }
