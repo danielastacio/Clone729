9d34ee2eab315b12e9d67c32e18a2eba666513bb
Daniel Astacio <danielastacio91@gmail.com>
2022-05-22 16:34:28 -0400

    Functionality for rolling and crouching

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,22 +8,59 @@ public class PlayerMovement : MonoBehaviour, IDamageable
 {
     [Header("Stats")]
     [SerializeField]
-    public float maxHp;
+    private float maxHp;
 
     [SerializeField]
-    public float currentHp;
+    private float currentHp;
 
     [SerializeField]
-    public float speed = 10;
+    private float speed = 0;
 
     [SerializeField]
-    private float jumpForce = 25 ;
+    private float defaultSpeed = 10;
+
+    [SerializeField]
+    private float crouchSpeed = 5;
+
+    [SerializeField]
+    private float jumpForce = 25;
+
+    [SerializeField]
+    private float rollForce = 1000;
+
+    [SerializeField]
     private bool _isGrounded;
+
+    [SerializeField]
+    private bool isCrouching, isRolling;
+
+    [SerializeField]
+    private float playerHeight;
+
+    [SerializeField]
+    private float crouchHeight;
+    private float horizontal;
+
+    [SerializeField]
+    private bool canMove;
+    [SerializeField]
+    private bool isMovingRight;
+    [SerializeField]
+    private bool isMovingLeft;
+
+    public bool isfacingRight;
+    public bool isfacingLeft;
+
+    [SerializeField]
+    private float rollTime;
+    [SerializeField]
+    private float defaultRollTime = 0.5f;
 
     [SerializeField]
     private float offsetRadius = -1.7f;
     // Cached References
     private Rigidbody2D _rb;
+
 
     [Header("Fall Variables")]
     [SerializeField]
@@ -40,11 +77,76 @@ public class PlayerMovement : MonoBehaviour, IDamageable
         _rb = GetComponent<Rigidbody2D>();
         _rb.gravityScale = 10;
         _rb.freezeRotation = true;
+
+        speed = defaultSpeed;
+        canMove = true;
+        playerHeight = transform.localScale.y;
+        crouchHeight = playerHeight / 2;
+        rollTime = defaultRollTime;
+
+        isfacingRight = true;
+        isfacingLeft = false;
+        
     }
 
     private void Update()
     {
 
+        if (Input.GetKey(KeyCode.S) && _isGrounded && !isRolling)
+        {
+            isCrouching = true;
+        }
+
+        else
+        {
+            isCrouching = false;
+        }
+
+        if(Input.GetKey(KeyCode.A) && !isRolling)
+        {
+            isMovingLeft = true;            
+            isfacingLeft = true;
+
+            isfacingRight = false;
+        }
+
+        else
+        {
+           isMovingLeft = false;
+        }
+
+        if (Input.GetKey(KeyCode.D) && !isRolling)
+        {
+            isMovingRight = true;
+            isfacingRight = true;
+
+            isfacingLeft = false;            
+        }
+
+        else
+        {
+            isMovingRight = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.K) && _isGrounded && !isRolling)
+        {
+            isRolling = true;
+        }
+
+        if (isRolling)
+        {            
+            rollTime -= Time.deltaTime;
+            if (rollTime <= 0 )
+            {
+                rollTime = 0;
+                isRolling = false;
+            }
+        }
+
+        else
+        {
+            rollTime = defaultRollTime;
+        }
     }
 
     void FixedUpdate()
@@ -52,14 +154,60 @@ public class PlayerMovement : MonoBehaviour, IDamageable
         Move();
         CheckIfGrounded();
         Jump();
+        Crouch();
+        Roll();
     }
-
     void Move()
     {
-        var horizontal = Input.GetAxisRaw("Horizontal") * speed;
+        if (canMove)
+        {
+            if (isMovingLeft)
+            {
+                _rb.velocity = new Vector2(-speed, _rb.velocity.y);
+            }
 
-        _rb.velocity = new Vector2(horizontal, _rb.velocity.y);
+            else if(isMovingRight)
+            {
+                _rb.velocity = new Vector2(speed, _rb.velocity.y);
+            }
+
+            else
+            {
+                _rb.velocity = new Vector2(0, _rb.velocity.y);
+            }
+
+        }
     }
+
+    void Crouch()
+    {
+        if (isCrouching)
+        {            
+            if (transform.localScale.y != crouchHeight)
+            {
+                transform.localScale = new Vector2(transform.localScale.x, crouchHeight);
+            }
+            speed = crouchSpeed;
+        }
+
+        else
+        {
+
+            transform.localScale = new Vector2(transform.localScale.x, playerHeight);
+            speed = defaultSpeed;
+        }
+    }
+
+    void Roll()
+    {
+        Vector2 rollDirection = isfacingLeft ? Vector2.left : Vector2.right;
+
+        if(isRolling)
+        {
+            _rb.AddForce(rollDirection * rollForce, ForceMode2D.Impulse);
+        }
+    }
+
 
     void CheckIfGrounded()
     {
@@ -78,7 +226,7 @@ public class PlayerMovement : MonoBehaviour, IDamageable
     {
         var jumpInput = Input.GetKey(KeyCode.Space);
 
-        if (jumpInput && _isGrounded)
+        if (jumpInput && _isGrounded && !isRolling)
         {
             _isGrounded = false;
             _rb.velocity = Vector2.up * jumpForce;
