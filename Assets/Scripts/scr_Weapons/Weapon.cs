32817f1a735c73d892fb6ea6715f9b323b186967
Tyler Ditlow <59483267+TylerD94@users.noreply.github.com>
2022-05-31 10:50:59 -0400

    Pulled new main

--- Assets/Scripts/scr_Weapons/Weapon.cs
+++ Assets/Scripts/scr_Weapons/Weapon.cs
@@ -1,62 +1,63 @@
 using System.Collections;
-using System.Collections.Generic;
-using scr_Weapons;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+namespace scr_Weapons
 {
-    //Inspector Dudes/Variables
-    [Header("Debug")]
-    [SerializeField][Tooltip("if gun isn't looking at cursor keep increasing offset")] int offset;
-    [Header("Bullet")]
-    [SerializeField] private GameObject bullet;
-    [SerializeField] private float bulletDamage;
-    [SerializeField] private float bulletSpeed;
-    [SerializeField] private float fireRate;
+    public class Weapon : MonoBehaviour
+    {
+        //Inspector Dudes/Variables
+        [Header("Debug")]
+        [SerializeField][Tooltip("if gun isn't looking at cursor keep increasing offset")] int offset;
+        [Header("Bullet")]
+        [SerializeField] private GameObject bullet;
+        [SerializeField] private float bulletDamage;
+        [SerializeField] private float bulletSpeed;
+        [SerializeField] private float fireRate;
 
-    //Private Dudes/Variables
-    private Vector3 difference;
-    private float angle;
-    private bool canShoot = true;
+        //Private Dudes/Variables
+        private Vector3 difference;
+        private float angle;
+        private bool canShoot = true;
     
-    private void Update()
-    {
-         LookAtCursor();
-         RotateAroundMech();
-        if(Input.GetMouseButtonUp(0) && canShoot)
+        private void Update()
         {
-            StartCoroutine(Shoot());
+            LookAtCursor();
+            RotateAroundMech();
+            if(Input.GetMouseButtonUp(0) && canShoot)
+            {
+                StartCoroutine(Shoot());
+            }
         }
-    }
 
-    private void LookAtCursor()
-    {
-        // Get Direction and Normalize It
-        difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        difference.Normalize();
-        //Get Rotation number
-        angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        //Rotate by said amount
-        transform.rotation = Quaternion.Euler(0f, 0f, angle + offset);
+        private void LookAtCursor()
+        {
+            // Get Direction and Normalize It
+            difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            difference.Normalize();
+            //Get Rotation number
+            angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            //Rotate by said amount
+            transform.rotation = Quaternion.Euler(0f, 0f, angle + offset);
 
-    }
-    private void RotateAroundMech()
-    {
-        //set Position around the mech by taking which direction it's pointing at
-        transform.position = transform.parent.position + difference;
-    }
+        }
+        private void RotateAroundMech()
+        {
+            //set Position around the mech by taking which direction it's pointing at
+            transform.position = transform.parent.position + difference;
+        }
 
-    protected virtual void  InstantiateBullet()
-    {
-        var newBullet = 
-            Instantiate(bullet, transform.position, Quaternion.Euler(0f, 0f, angle + offset));
-        newBullet.GetComponent<PlayerBullet>().CreateBullet("Enemy", bulletDamage, bulletSpeed);
-    }
-    IEnumerator Shoot()
-    {
-        canShoot = false;
-        InstantiateBullet();
-        yield return new WaitForSeconds(fireRate);
-        canShoot = true;
+        protected virtual void  InstantiateBullet()
+        {
+            var newBullet = 
+                Instantiate(bullet, transform.position, Quaternion.Euler(0f, 0f, angle + offset));
+            newBullet.GetComponent<PlayerBullet>().CreateBullet("Enemy", bulletDamage, bulletSpeed);
+        }
+        IEnumerator Shoot()
+        {
+            canShoot = false;
+            InstantiateBullet();
+            yield return new WaitForSeconds(fireRate);
+            canShoot = true;
+        }
     }
 }
