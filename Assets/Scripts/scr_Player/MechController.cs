fe8608c4171f05d40a1425b376e532f5cfb287c4
Daniel Astacio <danielastacio91@gmail.com>
2022-05-31 14:59:47 -0400

    Gameplay mechanics / Cinemachine / 2D rendering  (#9)
    * Refactored Mech and Player Controller code
    
    * Set Mech Controller script to be disabled by default
    
    * Added name spacing
    
    * Updated mech and player prefabs and their scripts
    
    Scale of Mech is now twice the size of the player. Fixed camera jitter by setting both rigidbodies to "continuous detection" and turned "Interpolation" on. Both scripts are have an offset and ground check radius set to default values. During run time, the mech script has logic to override the ground check radius to accommodate the mechs scale.
    
    * Created Camera prefab
    
    * Set "Main Camera" tag on main camera
    
    * Update Cameras.prefab
    
    Can't preset a follow or look at target. It has to be none, or it will cause weird issues.
    
    * Cleaned up input check logic
    
    * Created Launch player mechanic
    
    * Created asset files for 2D rendering and post-processing

--- Assets/Scripts/scr_Player/MechController.cs
+++ Assets/Scripts/scr_Player/MechController.cs
@@ -2,42 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MechController : PlayerController
+namespace MetroidvaniaJam.Player
 {
-    private void OnEnable()
+    public class MechController : PlayerController
     {
-        SetDefaultMechSettings();
-        MechActivated += ActivateMech;
-        MechDeactivated += DeactivateMech;
+        protected override void SetRigidbodySettings()
+        {            
+            rb = GetComponent<Rigidbody2D>();
+            rb.gravityScale = 2;
+
+            fallMultiplier = 17;
+            jumpForce = 12;
+
+            offsetRadius = base.offsetRadius * 2;
+            groundCheckRadius = base.groundCheckRadius * 2;
+        }
+
+        protected override void CheckCrouchInput() { }
+        protected override void CheckRollInput() { }
     }
-    private void ActivateMech()
-    {
-        UnFreezeRigidBody();
-        rb.GetComponent<CapsuleCollider2D>().isTrigger = false;
-    }
-
-    private void DeactivateMech()
-    {
-        FreezeRigidBody();
-        rb.GetComponent<CapsuleCollider2D>().isTrigger = true;
-    }
-    private void SetDefaultMechSettings()
-    {
-        fallMultiplier = 17;
-        jumpForce = 12;
-
-        offsetRadius = -4.5f;
-        groundCheckRadius = 1.5f;
-
-        rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = 2;
-
-        rb.GetComponent<CapsuleCollider2D>().isTrigger = true;
-    }
-
-    protected override void CheckCrouchInput() { }
-    protected override void CheckRollInput() { }
-    protected override void OnTriggerEnter2D(Collider2D collider) { }
-
-    protected override void OnTriggerExit2D(Collider2D collider) { }
 }
