d6ac8801c7cbc70d199ecef2b548895a0a9c36bd
Tyler Ditlow <59483267+TylerD94@users.noreply.github.com>
2022-06-01 10:59:38 -0400

    Fixed local breaks in bulletscript and weapon

--- Assets/Scripts/scr_Weapons/Weapon.cs
+++ Assets/Scripts/scr_Weapons/Weapon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using scr_Weapons;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
@@ -28,59 +29,11 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     protected virtual void Update()
     {
-        //Inspector Dudes/Variables
-        [Header("Debug")]
-        [SerializeField][Tooltip("if gun isn't looking at cursor keep increasing offset")] int offset;
-        [Header("Bullet")]
-        [SerializeField] private GameObject bullet;
-        [SerializeField] private float bulletDamage;
-        [SerializeField] private float bulletSpeed;
-        [SerializeField] private float fireRate;
-
-        //Private Dudes/Variables
-        private Vector3 difference;
-        private float angle;
-        private bool canShoot = true;
-
-        private void Update()
+        LookAtCursor();
+        RotateAroundMech();
+        if(Input.GetMouseButtonUp(0) && canShoot)
         {
-            LookAtCursor();
-            RotateAroundMech();
-            if(Input.GetMouseButtonUp(0) && canShoot)
-            {
-                StartCoroutine(Shoot());
-            }
-        }
-
-        private void LookAtCursor()
-        {
-            // Get Direction and Normalize It
-            difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            difference.Normalize();
-            //Get Rotation number
-            angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-            //Rotate by said amount
-            transform.rotation = Quaternion.Euler(0f, 0f, angle + offset);
-
-        }
-        private void RotateAroundMech()
-        {
-            //set Position around the mech by taking which direction it's pointing at
-            transform.position = transform.parent.position + difference;
-        }
-
-        protected virtual void  InstantiateBullet()
-        {
-            var newBullet =
-                Instantiate(bullet, transform.position, Quaternion.Euler(0f, 0f, angle + offset));
-            newBullet.GetComponent<PlayerBullet>().CreateBullet("Enemy", bulletDamage, bulletSpeed);
-        }
-        IEnumerator Shoot()
-        {
-            canShoot = false;
-            InstantiateBullet();
-            yield return new WaitForSeconds(fireRate);
-            canShoot = true;
+            StartCoroutine(Shoot());
         }
     }
 
