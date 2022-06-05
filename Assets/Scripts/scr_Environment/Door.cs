a61777869167da00bbc21000edbf77e9d1bdd85b
Tyler Ditlow <59483267+TylerD94@users.noreply.github.com>
2022-06-05 13:51:48 -0400

    Set up door prefab, tilemap

--- Assets/Scripts/scr_Environment/Door.cs
+++ Assets/Scripts/scr_Environment/Door.cs
@@ -7,33 +7,19 @@ namespace scr_Environment
 {
     public class Door : MonoBehaviour, IUnlockable
     {
-        private Vector2 _openPos;
-        private Vector2 _closedPos;
-        private float _doorSize;
+        private Animator _anim;
+        private BoxCollider2D _bc2d;
 
         private void Awake()
         {
-            _doorSize = transform.localScale.y;
-            _closedPos = transform.position;
-            _openPos = new Vector2(transform.position.x, transform.position.y + _doorSize);
+            _anim = gameObject.GetComponent<Animator>();
+            _bc2d = gameObject.GetComponent<BoxCollider2D>();
         }
 
         public void UnlockDoor()
         { 
-            StartCoroutine(OpenDoor());
-        }
-
-        private IEnumerator OpenDoor()
-        {
-            var transitionTime = 0f;
-
-            while (Vector2.Distance(transform.position, _openPos) >= 0.1)
-            {
-                Vector2 position = Vector2.Lerp(_closedPos, _openPos, transitionTime);
-                transform.position = position;
-                transitionTime += 0.1f;
-                yield return null;
-            }
+            _anim.SetBool("IsOpen", true);
+            _bc2d.enabled = false;
         }
     }
 }
