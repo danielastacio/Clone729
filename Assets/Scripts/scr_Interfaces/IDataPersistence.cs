4aa5df557403d8237cc3c82a96ddce7aac3bd8ae
Daniel Astacio <danielastacio91@gmail.com>
2022-06-07 16:27:16 -0400

    Create IDataPersistence.cs

---
+++ Assets/Scripts/scr_Interfaces/IDataPersistence.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace scr_Interface
+{ 
+ interface IDataPersistence
+    {
+        public void LoadData(GameData data);
+
+        public void SaveData(GameData data);
+    }
+}
