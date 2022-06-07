9fdb2f86bac3453e9c2ba598b87c3a2c542a0ad9
Daniel Astacio <danielastacio91@gmail.com>
2022-06-07 17:12:36 -0400

    Created Data Persistence Manager

---
+++ Assets/Scripts/scr_DataPersistence/DataPersistenceManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using scr_Interfaces;
+public class DataPersistenceManager : MonoBehaviour
+{
+    private GameData gameData;
+    private List<IDataPersistence> dataPersistenceObjects;
+    private FileDataHandler dataHandler;
+
+    public static DataPersistenceManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("Found more than one Data Persistence Manager in the scene.");
+        }
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Equals)) SaveGame();
+        if (Input.GetKeyDown(KeyCode.Minus)) LoadGame();
+    }
+
+    private void Start()
+    {
+        this.dataHandler = new FileDataHandler();
+        this.dataPersistenceObjects = FindAllDataPersistenceObjects();
+        LoadGame();
+    }
+
+    public void NewGame()
+    {
+        this.gameData = new GameData();
+    }
+
+    public void LoadGame()
+    {
+        // load any saved data from a file using the data handler
+        this.gameData = dataHandler.Load();
+
+        // if no data can be loaded, initialize to a new game
+        if (this.gameData == null)
+        {
+            Debug.Log("No data was found. Initializing data to defaults.");
+            NewGame();
+        }
+
+        // push the loaded data to all other scripts that need it
+        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+        {
+            dataPersistenceObj.LoadData(gameData);
+        }
+    }
+
+    public void SaveGame()
+    {
+        // pass the data to other scripts so they can update it
+        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+        {
+            dataPersistenceObj.SaveData(gameData);
+        }
+
+        // save that data to a file using the data handler
+        dataHandler.Save(gameData);
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame();
+    }
+
+    private List<IDataPersistence> FindAllDataPersistenceObjects()
+    {
+        IEnumerable<IDataPersistence> dataPersistenceObjects = FindObjectsOfType<MonoBehaviour>()
+            .OfType<IDataPersistence>();
+
+        return new List<IDataPersistence>(dataPersistenceObjects);
+    }
+}
