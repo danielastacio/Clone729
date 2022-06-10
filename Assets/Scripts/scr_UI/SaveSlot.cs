63bd4c74554912c5c7a50b2839f903000df92d9c
Daniel Astacio <danielastacio91@gmail.com>
2022-06-10 01:02:06 -0400

    Create SaveSlot.cs

---
+++ Assets/Scripts/scr_UI/SaveSlot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class SaveSlot : MonoBehaviour
+{
+    DataPersistenceManager dataManager;
+    [SerializeField] private DataPersistenceManager.ProfileIds profileId;
+    private GameData gameData;
+    private Button slotButton;
+    // Use this for initialization
+    void Awake()
+    {        
+        slotButton = GetComponent<Button>();
+        
+    }
+
+    void Start()
+    {
+        LoadProfileData();
+
+        if (gameData == null)
+        {
+            slotButton.interactable = false;
+        }
+
+        else
+        {
+            slotButton.interactable = true;
+        }
+    }
+    public void LoadProfileData()
+    { 
+        DataPersistenceManager.Instance.GetAllProfiles().TryGetValue(profileId.ToString(), out gameData);
+    }
+}
