16c4ad55a24c0033ce0d415240079b7dcc4021f8
Daniel Astacio <danielastacio91@gmail.com>
2022-06-11 11:31:52 -0400

    Update SaveSlot.cs

--- Assets/Scripts/scr_UI/SaveSlot.cs
+++ Assets/Scripts/scr_UI/SaveSlot.cs
@@ -4,33 +4,49 @@ using TMPro;
 using UnityEngine.UI;
 public class SaveSlot : MonoBehaviour
 {
-    DataPersistenceManager dataManager;
     [SerializeField] private DataPersistenceManager.ProfileIds profileId;
+
     private GameData gameData;
     private Button slotButton;
+    [SerializeField] private bool isGameLoading;
     // Use this for initialization
     void Awake()
-    {        
+    {
         slotButton = GetComponent<Button>();
-        
+        slotButton.onClick.AddListener(OnSlotButtonClicked);
     }
 
     void Start()
     {
-        LoadProfileData();
+        //LoadProfileData();
 
-        if (gameData == null)
+        //if (gameData == null)
+        //{
+        //    slotButton.interactable = false;
+        //}
+
+        //else
+        //{
+        //    slotButton.interactable = true;
+        //}
+    }    
+    public void LoadProfileData()
+    {
+        DataPersistenceManager.Instance.LoadProfiles().TryGetValue(profileId.ToString(), out gameData);
+    }
+
+    private void OnSlotButtonClicked()
+    {
+        DataPersistenceManager.Instance.ChangeSelectedProfileId(profileId);
+
+        if (isGameLoading)
         {
-            slotButton.interactable = false;
+            DataPersistenceManager.Instance.LoadGame();
         }
 
         else
         {
-            slotButton.interactable = true;
+            DataPersistenceManager.Instance.SaveGame();
         }
-    }
-    public void LoadProfileData()
-    { 
-        DataPersistenceManager.Instance.GetAllProfiles().TryGetValue(profileId.ToString(), out gameData);
     }
 }
