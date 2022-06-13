83716777020357a8a166df58765dfdaae4bb0c77
Daniel Astacio <danielastacio91@gmail.com>
2022-06-13 09:08:33 -0400

    Create MainMenu.cs

---
+++ Assets/Scripts/scr_UI/Scr_MainMenu/MainMenu.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Menu Navigation")]
+    [SerializeField] private SaveSlotsMenu saveSlotsMenu;
+
+    [Header("Menu Buttons")]
+    [SerializeField] private Button newGameButton;
+    [SerializeField] private Button continueGameButton;
+    [SerializeField] private Button loadGameButton;
+
+    private void Start() 
+    {
+        if (!DataPersistenceManager.instance.HasGameData()) 
+        {
+            continueGameButton.interactable = false;
+            loadGameButton.interactable = false;
+        }
+    }
+
+    public void OnNewGameClicked() 
+    {
+        saveSlotsMenu.ActivateMenu(false);
+        this.DeactivateMenu();
+    }
+
+    public void OnLoadGameClicked() 
+    {
+        saveSlotsMenu.ActivateMenu(true);
+        this.DeactivateMenu();
+    }
+
+    public void OnContinueGameClicked() 
+    {
+        DisableMenuButtons();
+        // load the next scene - which will in turn load the game because of 
+        // OnSceneLoaded() in the DataPersistenceManager
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    private void DisableMenuButtons() 
+    {
+        newGameButton.interactable = false;
+        continueGameButton.interactable = false;
+    }
+
+    public void ActivateMenu() 
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void DeactivateMenu() 
+    {
+        this.gameObject.SetActive(false);
+    }
+}
