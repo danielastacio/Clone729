b4ad8b0fa9e69dd48b0cb36b7521c9331229cf81
Tyler Ditlow <59483267+TylerD94@users.noreply.github.com>
2022-06-14 10:15:40 -0400

    Fixed local conflicts after merge, going to create new branch from these changes. (#25)
    * Added a canvas to pause menu to display current stats
    
    Pause menu canvas currently using TV asset, may want to change this later.
    
    * Updated scene
    
    * Set up static colors class, as well as ability for each pause menu button to open its own canvas.
    
    * Cleaned up scripts folders
    
    * Created static CanvasController class for canvas-specific methods
    
    * Removed duplicate directory and scripts from merge conflict
    
    * Fixed conflicts in MenuButtonHover.cs
    
    * Fixed local conflicts

--- Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
+++ Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
@@ -7,45 +7,19 @@ using UnityEngine.UI;
 
 namespace scr_UI.scr_PauseMenu
 {
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
     public class PauseMenu : MonoBehaviour
-========
-    public class MainMenuButtons : MonoBehaviour
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
-========
-    public class MainMenuButtons : MonoBehaviour
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
     {
         public List<Button> buttons = new List<Button>();
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
         public List<Canvas> canvases = new List<Canvas>();
         private readonly List<Vector2> _buttonStartingPositions = new List<Vector2>();
-========
-        private List<Vector2> _buttonStartingPositions = new List<Vector2>();
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
-========
-        private List<Vector2> _buttonStartingPositions = new List<Vector2>();
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
         private Button _selectedButton;
 
         private readonly Vector2 _openMenuPos = new Vector2(394, 350);
         private Vector2 _hiddenPos;
-
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
+        
         [SerializeField] private Canvas statsCanvas;
 
         [SerializeField] private float buttonMoveTime;
-========
-        protected readonly Color32 DefaultColor = new Color32(159, 159, 159, 255);
-        protected readonly Color32 SelectedColor = new Color32(255, 255, 255, 255);
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
-========
-        protected readonly Color32 DefaultColor = new Color32(159, 159, 159, 255);
-        protected readonly Color32 SelectedColor = new Color32(255, 255, 255, 255);
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
 
         public void Start()
         {
@@ -66,15 +40,7 @@ namespace scr_UI.scr_PauseMenu
                         _selectedButton = button;
                         if (_selectedButton.name.Equals("ExitButton"))
                         {
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
                             CloseMenu();
-========
-                            Application.Quit();
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
-========
-                            Application.Quit();
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
                         }
                         else if (_selectedButton != null)
                         {
@@ -89,34 +55,16 @@ namespace scr_UI.scr_PauseMenu
 
             if (Input.GetKeyDown(KeyCode.Escape) && _selectedButton != null)
             {
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
                 ResetButtonPositions();
             }
             else if (Input.GetKeyDown(KeyCode.Escape) && _selectedButton == null)
             {
                 CloseMenu();
             }
-========
-                _selectedButton.GetComponent<MainMenuButtons>().enabled = true;
-                _selectedButton = null;
-                ResetButtonPositions();
-            }
-            
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
-========
-                _selectedButton.GetComponent<MainMenuButtons>().enabled = true;
-                _selectedButton = null;
-                ResetButtonPositions();
-            }
-            
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
         }
 
         private void CloseMenu()
         {
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_PauseMenu/PauseMenu.cs
             Time.timeScale = 1;
             gameObject.SetActive(false);
         }
@@ -126,14 +74,6 @@ namespace scr_UI.scr_PauseMenu
             btn.image.color = Colors.HighlightedMenuButtonColor;
             _selectedButton.GetComponent<MenuButtonHover>().enabled = false;
             CanvasController.ShowCanvas(canvases[buttons.IndexOf(btn)]);
-========
-            btn.image.color = SelectedColor;
-            _selectedButton.GetComponent<MainMenuButtons>().enabled = false;
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
-========
-            btn.image.color = SelectedColor;
-            _selectedButton.GetComponent<MainMenuButtons>().enabled = false;
->>>>>>>> dev:Assets/Scripts/scr_UI/MainMenuButtons.cs
             StartCoroutine(MoveButtons(btn, _openMenuPos));
         }
 
