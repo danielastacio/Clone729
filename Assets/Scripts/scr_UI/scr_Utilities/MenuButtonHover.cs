4f381468a641090f2c6f86def29193ea14def8b8
Tyler Ditlow <59483267+TylerD94@users.noreply.github.com>
2022-06-14 09:48:00 -0400

    Fixed conflicts in MenuButtonHover.cs

--- Assets/Scripts/scr_UI/scr_Utilities/MenuButtonHover.cs
+++ Assets/Scripts/scr_UI/scr_Utilities/MenuButtonHover.cs
@@ -1,27 +1,20 @@
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_Utilities/MenuButtonHover.cs
 using UnityEngine;
-========
->>>>>>>> origin/dev:Assets/Scripts/scr_UI/MenuButtonHover.cs
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace scr_UI.scr_Utilities
 {
-<<<<<<<< HEAD:Assets/Scripts/scr_UI/scr_Utilities/MenuButtonHover.cs
     public class MenuButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
-========
-    public class MenuButtonHover : MainMenuButtons, IPointerEnterHandler, IPointerExitHandler
->>>>>>>> origin/dev:Assets/Scripts/scr_UI/MenuButtonHover.cs
     {
         public void OnPointerEnter(PointerEventData eventData)
         {
-            gameObject.GetComponent<Button>().image.color = SelectedColor;
+            gameObject.GetComponent<Button>().image.color = Colors.HighlightedMenuButtonColor;
         }
         
         
         public void OnPointerExit(PointerEventData eventData)
         {
-            gameObject.GetComponent<Button>().image.color = DefaultColor;
+            gameObject.GetComponent<Button>().image.color = Colors.DefaultMenuButtonColor;
         }
     }
 }
