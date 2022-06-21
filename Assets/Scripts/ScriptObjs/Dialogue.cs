a56bca07a39aed522524b01bb681131cb40efcbd
Tyler Ditlow <59483267+TylerD94@users.noreply.github.com>
2022-06-21 10:31:40 -0400

    Branch off of Dialogue-System PRE-MERGE

--- Assets/Scripts/ScriptObjs/Dialogue.cs
+++ Assets/Scripts/ScriptObjs/Dialogue.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace ScriptObjs
@@ -13,7 +15,38 @@ namespace ScriptObjs
     [CreateAssetMenu (menuName = "New Dialogue", fileName = "New Dialogue")]
     public class Dialogue : ScriptableObject
     {
+        [CustomEditor(typeof(Dialogue)), CanEditMultipleObjects]
+        public class DialogueEditor : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                base.OnInspectorGUI();
+                var dialogue = target as Dialogue;
+
+                using (var group = new EditorGUILayout.FadeGroupScope(Convert.ToSingle(dialogue._isInteractable)))
+                {
+                    if (dialogue._isInteractable)
+                    {
+                        EditorGUI.indentLevel++;
+                        EditorGUILayout.PrefixLabel("Confirm");
+                        dialogue.confirm = EditorGUILayout.TextField(dialogue.confirm);
+                        EditorGUILayout.PrefixLabel("Decline");
+                        dialogue.decline = EditorGUILayout.TextField(dialogue.decline);
+                    }
+                }
+            }
+        }
+        
         public List<string> dialogueStrings = new();
         public DialogueType dialogueType;
+        
+        private bool _isInteractable;
+        [HideInInspector] public string confirm;
+        [HideInInspector] public string decline;
+
+        private void OnValidate()
+        {
+            _isInteractable = dialogueType == DialogueType.InteractableTextBox;
+        }
     }
 }
