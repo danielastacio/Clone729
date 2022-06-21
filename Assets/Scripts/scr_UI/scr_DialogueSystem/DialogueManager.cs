a65b3b8897643c8c449de03d0916b72ba08d9fce
Tyler Ditlow <59483267+TylerD94@users.noreply.github.com>
2022-06-21 10:57:59 -0400

    Dialogue system is unmanagable. Commiting here before attempting to merge.

--- Assets/Scripts/scr_UI/scr_DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/scr_UI/scr_DialogueSystem/DialogueManager.cs
@@ -6,13 +6,19 @@ using scr_UI.scr_Utilities;
 using ScriptObjs;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace scr_UI.scr_DialogueSystem
 {
     public class DialogueManager : MonoBehaviour
     {
         [SerializeField] private Canvas dialogueCanvas;
+        [SerializeField] private Canvas interactableDialogueCanvas;
         [SerializeField] private TextMeshProUGUI dialogueText;
+        [SerializeField] private Button confirmButton;
+        [SerializeField] private TextMeshProUGUI confirmText;
+        [SerializeField] private Button declineButton;
+        [SerializeField] private TextMeshProUGUI declineText;
 
         private bool _dialogueEnabled = false;
         private IEnumerator _activeDialogue = null;
@@ -31,20 +37,34 @@ namespace scr_UI.scr_DialogueSystem
             Actions.OnTextSpeedChanged -= SetTextSpeed;
         }
 
-        private void DisplayDialogue(NPCDialogue inputDialogue)
+        private void DisplayDialogue(NPCDialogue dialogueObj)
         {
-            var currentDialogue = inputDialogue.dialogueStrings[inputDialogue.timesInteracted];
-            inputDialogue.IncreaseInteractCount();
+            var currentDialogue = dialogueObj.dialogueStrings[dialogueObj.timesInteracted];
+            dialogueObj.IncreaseInteractCount();
             if (currentDialogue.dialogueType == DialogueType.TextBox)
             {
                 DisplayDialogueBox(currentDialogue);
+            }
+            else if (currentDialogue.dialogueType == DialogueType.InteractableTextBox)
+            {
+                DisplayInteractableDialogueBox(dialogueObj, currentDialogue);
             }
         }
 
         private void DisplayDialogueBox(Dialogue inputDialogue)
         {
             _dialogueEnabled = true;
-            _activeDialogue = UpdateDialogText(inputDialogue.dialogueStrings);
+            _activeDialogue = UpdateDialogText(inputDialogue.dialogueStrings, dialogueCanvas);
+            if (_activeDialogue != null)
+            {
+                StartCoroutine(_activeDialogue);
+            }
+        }
+
+        private void DisplayInteractableDialogueBox(NPCDialogue dialogueObj, Dialogue inputDialogue)
+        {
+            _dialogueEnabled = true;
+            _activeDialogue = UpdateDialogText(inputDialogue, inputDialogue.dialogueStrings, interactableDialogueCanvas);
             if (_activeDialogue != null)
             {
                 StartCoroutine(_activeDialogue);
@@ -58,12 +78,12 @@ namespace scr_UI.scr_DialogueSystem
             CanvasController.HideCanvas(dialogueCanvas);
         }
 
-        private IEnumerator UpdateDialogText(List<string> dialogue)
+        private IEnumerator UpdateDialogText(List<string> dialogue, Canvas canvas)
         {
             var currentString = 0;
             char[] currentSentence = dialogue[currentString].ToCharArray();
 
-            CanvasController.ShowCanvas(dialogueCanvas);
+            CanvasController.ShowCanvas(canvas);
             if (_activePrinter == null)
             {
                 _activePrinter = PrintDialogue(currentSentence);
@@ -72,6 +92,42 @@ namespace scr_UI.scr_DialogueSystem
             
             while (_dialogueEnabled)
             {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    StopCoroutine(_activePrinter);
+                    currentString++;
+                    if (currentString == dialogue.Count)
+                    {
+                        HideDialog();
+                        _activePrinter = null;
+                        yield break;
+                    }
+                    currentSentence = dialogue[currentString].ToCharArray();
+                    _activePrinter = PrintDialogue(currentSentence);
+                    yield return StartCoroutine(_activePrinter);
+                }
+                yield return null;
+            }
+        }
+        
+        private IEnumerator UpdateDialogText(Dialogue dObj, List<string> dialogue, Canvas canvas)
+        {
+            var currentString = 0;
+            char[] currentSentence = dialogue[currentString].ToCharArray();
+            confirmText.text = dObj.confirm;
+            declineText.text = dObj.decline;
+
+            CanvasController.ShowCanvas(canvas);
+            if (_activePrinter == null)
+            {
+                _activePrinter = PrintDialogue(currentSentence);
+                StartCoroutine(_activePrinter);
+            }
+            
+            while (_dialogueEnabled)
+            {
+                confirmButton.onClick.AddListener(() => Debug.Log("Confirm Pressed!"));
+                declineButton.onClick.AddListener(() => Debug.Log("Decline Pressed"));
                 if (Input.GetKeyDown(KeyCode.E))
                 {
                     StopCoroutine(_activePrinter);
